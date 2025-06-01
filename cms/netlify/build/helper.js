6236e5ea4e2aba2389966f654b6a7feb49ccb02e
Sylvester Carolan <sylvestercarolan@gmail.com>
2025-06-01 19:54:56 -0400

    updated depreciated warnings

--- cms/netlify/build/helper.js
+++ cms/netlify/build/helper.js
@@ -43,41 +43,27 @@ export function createMeta(newMeta, file) {
   chunkWriteStream.write(JSON.stringify(combined))
 }
 export function createAll(fromDir, toFile, apiDir) {
-  // Create api dir if doesn't exist
   if (!fs.existsSync(apiDir)) {
-    fs.mkdirSync(apiDir)
+    fs.mkdirSync(apiDir, { recursive: true })
   }
-  // Create file if doesn't exist
-  if (!fs.existsSync(toFile)) {
-    fs.writeFileSync(toFile, '{}')
-  }
-  return new Promise((resolve, reject) => {
-    fs.readdir(fromDir, (err, files) => {
-      if (err) reject(err)
-      const index = []
-      const contents = {}
-      files.forEach((file) => {
-        contents[file] = ''
-        const readStream = fs.createReadStream(`${fromDir}/${file}`, 'UTF-8')
-        readStream.on('data', (data, err) => {
-          if (err) throw err
-          contents[file] += data
-        })
-        readStream.on('end', () => {
-          const parsed = matter(contents[file])
-          delete parsed.buffer
-          parsed.data.slug = file.replace(/.md$/, '')
-          index.push(parsed)
 
-          if (index.length === files.length) {
-            const writeStream = fs.createWriteStream(toFile, 'UTF-8')
-            let sorted = index.sort(compareDates).reverse()
-            sorted = flattenResource(sorted)
-            writeStream.write(JSON.stringify(sorted))
-            resolve(sorted)
-          }
-        })
-      })
-    })
+  return new Promise(async (resolve, reject) => {
+    try {
+      const files = await fs.promises.readdir(fromDir)
+      const contents = await Promise.all(files.map(async file => {
+        const content = await fs.promises.readFile(`${fromDir}/${file}`, 'utf8')
+        const parsed = matter(content)
+        if (!parsed.data) parsed.data = {}  // Initialize if undefined
+        parsed.data.slug = file.replace(/.md$/, '')
+        return parsed
+      }))
+      
+      const sorted = contents.sort(compareDates).reverse()
+      const flattened = flattenResource(sorted)
+      await fs.promises.writeFile(toFile, JSON.stringify(flattened))
+      resolve(flattened)
+    } catch (err) {
+      reject(err)
+    }
   })
 }
