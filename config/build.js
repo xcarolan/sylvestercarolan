c3df7bf75c761c666cacf16170f7a9ab3f8f0849
Sylvester Carolan <sylvestercarolan@gmail.com>
2025-06-01 20:02:02 -0400

    another fix

--- config/build.js
+++ config/build.js
@@ -18,45 +18,47 @@ if (process.env.NODE_ENV === 'production') {
         customProperties: false
       }
     },
-    plugins: [
-      purgecss({
-        content: [
-          './pages/**/*.vue',
-          './layouts/**/*.vue',
-          './components/**/*.vue'
-        ],
-        css: css,
-        whitelist: [
-          'html',
-          'body',
-          'is-1by1',
-          'is-5by4',
-          'is-5by4',
-          'is-4by3',
-          'is-3by2',
-          'is-5by3',
-          'is-16by9',
-          'is-2by1',
-          'is-3by1',
-          'is-4by5',
-          'is-3by4',
-          'is-2by3',
-          'is-3by5',
-          'is-9by16',
-          'nuxt__build_indicator',
-          '__nuxt',
-          'svg',
-          'table',
-          'td',
-          'th',
-          'tr',
-          'tbody',
-          'thead',
-          'tfoot'
-        ],
-        whitelistPatterns: [/theme/g, /spinner-position/, /fa/, /table/g]
-      })
-    ]
+    postcssOptions: {
+      plugins: [
+        purgecss({
+          content: [
+            './pages/**/*.vue',
+            './layouts/**/*.vue',
+            './components/**/*.vue'
+          ],
+          css: css,
+          whitelist: [
+            'html',
+            'body',
+            'is-1by1',
+            'is-5by4',
+            'is-5by4',
+            'is-4by3',
+            'is-3by2',
+            'is-5by3',
+            'is-16by9',
+            'is-2by1',
+            'is-3by1',
+            'is-4by5',
+            'is-3by4',
+            'is-2by3',
+            'is-3by5',
+            'is-9by16',
+            'nuxt__build_indicator',
+            '__nuxt',
+            'svg',
+            'table',
+            'td',
+            'th',
+            'tr',
+            'tbody',
+            'thead',
+            'tfoot'
+          ],
+          whitelistPatterns: [/theme/g, /spinner-position/, /fa/, /table/g]
+        })
+      ]
+    }
   }
 }
 
